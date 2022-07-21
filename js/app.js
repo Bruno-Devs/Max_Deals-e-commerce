25e0fc6fa0432c4a5a59e8ad92eb323bb6ec79c9
Brunomax1997 <brunougwu568@gmail.com>
2022-07-21 18:12:35 +0100

    add my project

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar) {
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active');
+    })
+};
+
+if (close) {
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active');
+    })
+};
+
+let MainImg = document.getElementById("MainImg");
+let smallImg = document.getElementsByClassName("small-img");
+
+
+for (let i = 0; i < smallImg.length; i++) {
+    smallImg[i].onclick = function() {
+        MainImg.src = smallImg[i].src;
+    } 
+    
+}
+// let i = 0;
+// smallImg[i].onclick = function() {
+//     MainImg.src = smallImg[i].src;
+// }
+// i++
